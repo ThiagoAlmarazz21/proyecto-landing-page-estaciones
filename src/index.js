e8a5370e74bfbff99fcdbd99b145d39a4ce2974e
ThiagoAlmarazz21 <115847063+ThiagoAlmarazz21@users.noreply.github.com>
2023-06-07 00:21:16 -0300

    CASI TERMINADO, FALTA AGREGAR ALGO DE JAVASCRIPT

---
+++ src/index.js
@@ -0,0 +1,32 @@
+// SWIPER SLIDER 
+var swiper = new Swiper(".bg-slider-thumbs", {
+  loop: true,
+  spaceBetween: 0,
+  slidesPerView: 0,
+});
+var swiper2 = new Swiper(".bg-slider", {
+  loop: true,
+  spaceBetween: 0,
+  thumbs: {
+    swiper: swiper,
+  },
+});
+
+// NAVIGATION BAR EFFECTS ON SCROLL 
+window.addEventListener('scroll', function(){
+  const header = document.querySelector('header');
+  header.classList.toggle('sticky', this.window.scrollY > 0);
+});
+
+// RESPONSIVE NAVIGATION MENU TOGGLE 
+const menuBtn = document.getElementById('nav-menu-btn');
+const closeBtn = document.getElementById('nav-close-btn');
+const navigation = document.querySelector('.navigation')
+
+menuBtn.addEventListener('click', () => {
+  navigation.classList.add('active');
+});
+
+closeBtn.addEventListener('click', () => {
+  navigation.classList.remove('active');
+});
